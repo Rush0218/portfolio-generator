0823ee09faeccb330e658e474c7aa38fb74feb37
solera2 <rushtre1@gmail.com>
2021-11-30 14:58:19 -0500

    added function to generate html page

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
+const fs = require('fs'); 
+const generatePage = require('./src/page-template.js'); 
 const profileDataArgs = process.argv.slice(2); 
+const [name1, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    // this...
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]); 
-    }
-    console.log("=============="); 
-    // is the same as this..
-    profileDataArr.forEach((profileItem) => console.log(profileItem)); 
-}; 
 
-printProfileData(profileDataArgs); 
+// const printProfileData = profileDataArr => {
+//     // this...
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]); 
+//     }
+//     console.log("=============="); 
+//     // is the same as this..
+//     profileDataArr.forEach((profileItem) => console.log(profileItem)); 
+// }; 
+
+// printProfileData(profileDataArgs); 
+
+
+
+fs.writeFile('index.html', generatePage(name1, github), err => {
+    if(err) throw err; 
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+}); 
