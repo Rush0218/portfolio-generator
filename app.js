c3dae0fd77d5d060c49d219626316953480e2653
solera2 <rushtre1@gmail.com>
2021-11-30 17:43:37 -0500

    added function to take user input

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
-const fs = require('fs'); 
-const generatePage = require('./src/page-template.js'); 
-const profileDataArgs = process.argv.slice(2); 
-const [name1, github] = profileDataArgs;
+const inquirer = require('inquirer'); 
+const { type } = require('os');
+// const fs = require('fs'); 
+// const generatePage = require('./src/page-template.js'); 
 
+// const pageHTML = generatePage(name1, github); 
 
-// const printProfileData = profileDataArr => {
-//     // this...
-//     for (let i = 0; i < profileDataArr.length; i++) {
-//         console.log(profileDataArr[i]); 
-//     }
-//     console.log("=============="); 
-//     // is the same as this..
-//     profileDataArr.forEach((profileItem) => console.log(profileItem)); 
-// }; 
+// fs.writeFile('index.html', pageHTML, err => {
+//     if(err) throw err; 
 
-// printProfileData(profileDataArgs); 
-
-
-
-fs.writeFile('index.html', generatePage(name1, github), err => {
-    if(err) throw err; 
-
-    console.log('Portfolio complete! Check out index.html to see the output!')
-}); 
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// }); 
+inquirer
+    .prompt ([ 
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers)); 
