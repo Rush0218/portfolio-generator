855a7eb895ea229dd218577e70c2bf5d2669455f
solera2 <rushtre1@gmail.com>
2021-11-30 22:50:27 -0500

    cleaned up code

--- app.js
+++ app.js
@@ -2,14 +2,6 @@ const fs = require('fs');
 const inquirer = require('inquirer'); 
 const generatePage = require('./src/page-template.js'); 
 
-// const pageHTML = generatePage(name1, github); 
-
-// fs.writeFile('index.html', pageHTML, err => {
-//     if(err) throw err; 
-
-//     console.log('Portfolio complete! Check out index.html to see the output!')
-// }); 
-
 const promptUser = () => {
     return inquirer.prompt([ 
             {
